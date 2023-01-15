f922c03cc9f27b8e07c1bc9e852e558bd29ac835
Yohanna Joseph Waliya <11397428+yohannawaliya@users.noreply.github.com>
2023-01-15 15:43:23 +0100

    Add files via upload

--- randomlinks.js
+++ randomlinks.js
@@ -1,1 +1,17 @@
-randomlinks.js
+<script>
+	start();
+	function start(){
+	markanddanah ("meme.html", "astiwere.html", 
+	"autobio.html"[...]);
+
+}
+function markanddanah(){
+var argv=markanddanah.arguments;
+var argc=argv.length;
+var num=Math.floor(Math.random()*argc);
+document.write("<A HREF=", argv[num],">");
+return argv[0];
+
+
+}
+</script>
